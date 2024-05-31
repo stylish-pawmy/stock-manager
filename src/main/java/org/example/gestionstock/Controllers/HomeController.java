53c81438bdbb49c894a077862f3bb7f1a95b6e45
stylish-pawmy <ambesi2026@gmail.com>
2024-05-31 23:35:15 +0100

    add temporary home page

---
+++ src/main/java/org/example/gestionstock/Controllers/HomeController.java
@@ -0,0 +1,22 @@
+package org.example.gestionstock.Controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/")
+    public String getHome(Model page)
+    {
+         UserDetails userDetails = (UserDetails) SecurityContextHolder
+                 .getContext().getAuthentication().getPrincipal();
+
+        page.addAttribute("user", userDetails.getUsername());
+        return "dashboard";
+    }
+}
